3e3252a66ee00939099c95d67c9eaed2ba996f69
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-05-28 23:13:08 +0100

    added more tests

--- photoViewer/src/App.test.tsx
+++ photoViewer/src/App.test.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import App from './App';
 import { ImageSelector, imageUrls, brokenImages } from './PhotoViewer/ImageSelector';
 
-test('renders React Photo Viewer', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/React Photo Viewer/i);
-  expect(textElement).toBeVisible();
+test('getImageUrls does not include broken images', () => {
+    expect(imageUrls.length).toBe(44);
 });
 
-// A unit test to check our imageUrl generation code - for me, this might check that the first link is what I expect and that it doesn’t include the ‘broken’ images.
+test('render matches initial snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+}) 
+
+test('renders correct headings', () => {
+  const { getByText } = render(<App />);
+  const pageHeading = getByText(/React Photo Viewer/i);
+  const pageSubHeading = getByText(/Select your photo/i);
+  expect(pageHeading).toBeVisible();
+  expect(pageSubHeading).toBeVisible();
+});
+
 test('does not render non-broken image URL', () => {
     const { container } = render(<App />);
-
     const image = container.querySelector('img[src="https://picsum.photos/id/6100.jpg"]');
     expect(image).not.toBeInTheDocument();
 });
 
-test('test getURLImage method: not include broken image', () => {
-    let url;
-    let imageNumberString;
+test('initially renders main image as first element in imageUrls Array', async () => {
+    const { container } = render(<App />);
+    const mainImage = container.querySelector('.photo-viewer-main img');
+    expect(mainImage).toHaveAttribute('src', "https://picsum.photos/id/600/1600/900.jpg");
+});
 
-    brokenImages.forEach((image: number)=>{
-        imageNumberString = image.toString().padStart(2, '0');
-        url=`https://picsum.photos/id/6${imageNumberString}.jpg`;
-        expect(imageUrls).not.toContain(url);
-    })
+
+test('selected thumbnail updates ImageViewer(main image)', async () => {
+    const { container } = render(<App />);
+    const selectedImage = container.querySelector('img[src="https://picsum.photos/id/603/1600/900.jpg"]');
+    if (selectedImage) {
+    await userEvent.click(selectedImage);
+    }
+    expect(selectedImage).toHaveClass('highlightImage');
+    const mainImage = container.querySelector('.photo-viewer-main img');
+    expect(mainImage).toHaveAttribute('src', "https://picsum.photos/id/603/1600/900.jpg");
+    expect(container).toMatchSnapshot();
 });
